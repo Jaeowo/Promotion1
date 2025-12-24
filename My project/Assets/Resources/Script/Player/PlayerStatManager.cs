420b1eb5c1474138fbc5c9d559537d1595ba4f36
LeeJaehee <117854534+Jaeowo@users.noreply.github.com>
2025-12-24 12:05:34 +0900

    PlayerStatManager 추가

--- My project/Assets/Resources/Script/Player/PlayerStatManager.cs
+++ My project/Assets/Resources/Script/Player/PlayerStatManager.cs
@@ -2,13 +2,143 @@ using UnityEngine;
 
 public class PlayerStatManager : MonoBehaviour
 {
-    void Start()
+    [Header("Basic Stat")]
+    [SerializeField] private float maxHP = 100f;
+    [SerializeField] private float currentHP = 100f;
+    [SerializeField] private float attack = 3f;
+    [SerializeField] private float defence = 3f;
+
+    [Header("Probability")]
+    [SerializeField] private float criticalProbability = 0.1f;
+
+    [Header("Multiplier")]
+    [SerializeField] private float skillMultiplier = 1.2f;
+    [SerializeField] private float criticalMultiplier = 1.2f;
+
+    public static PlayerStatManager instance;
+
+    private void Awake()
     {
-        
+        // Singleton Setting
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    void Update()
+    #region Getter
+
+    public float MaxHP => maxHP;
+    public float CurrentHP => currentHP;
+    public float ATK => attack;
+    public float DEF => defence;
+    public float CriticalProbability => criticalProbability;
+    public float SkillMultiplier => skillMultiplier;
+    public float CriticalMultiplier => criticalMultiplier;
+
+    #endregion
+
+    #region BasicStat Setter
+
+    public void IncreaseMaxHP(float value)
     {
-        
+        maxHP += value;
+        currentHP += value;
     }
+
+    public void DecreaseMaxHP(float value)
+    {
+        maxHP -= value;
+        if (maxHP < 0f)
+            maxHP = 1f;
+
+        if (currentHP > maxHP)
+            currentHP = maxHP;
+    }
+
+    public void IncreaseCurrentHP(float value)
+    {
+        currentHP += value;
+        if (currentHP > maxHP)
+            currentHP = maxHP;
+    }
+
+    public void DecreaseCurrentHP(float value)
+    {
+        currentHP -= value;
+        if (currentHP < 0f)
+            currentHP = 0f;
+    }
+
+    public void IncreaseATK(float value)
+    {
+        attack += value;
+    }
+
+    public void DecreaseATK(float value)
+    {
+        attack -= value;
+    }
+
+    public void IncreaseDEF(float value)
+    {
+        defence += value;
+    }
+
+    public void DecreaseDEF(float value)
+    {
+        defence -= value;
+    }
+
+    #endregion
+
+    #region Probability Setter
+
+    public void IncreaseCriticalProbability(float value)
+    {
+        criticalProbability += value;
+
+        if (criticalProbability > 1.0f)
+        {
+            criticalProbability = 1.0f;
+        }
+    }
+
+    public void DecreaseCriticalProbability(float value)
+    {
+        criticalProbability -= value;
+
+        if (criticalProbability < 0.0f)
+        {
+            criticalProbability = 0.0f;
+        }
+    }
+    #endregion
+
+    #region Multiplier Setter
+    public void IncreaseSkillMultiplier(float value)
+    {
+        skillMultiplier += value;
+    }
+
+    public void DecreaseSkillMultiplier(float value)
+    {
+        skillMultiplier -= value;
+    }
+
+    public void IncreaseCriticalMultiplier(float value)
+    {
+        criticalMultiplier += value;
+    }
+
+    public void DecreaseCriticalMultiplier(float value)
+    {
+        criticalMultiplier -= value;
+    }
+    #endregion
 }
